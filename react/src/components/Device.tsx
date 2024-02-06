89d55908ddfeeec54bc09d421defd9cc409a67fe
krzymich24 <krzysztof.michalak1999@gmail.com>
2024-02-06 22:10:14 +0100

    Update Device.tsx

--- react/src/components/Device.tsx
+++ react/src/components/Device.tsx
@@ -1,11 +1,32 @@
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { DeviceCardProps } from './DeviceCard';
+import { DeviceCard, DeviceCardProps } from "./DeviceCard";
 import { API } from '../api';
 import { useAuth } from '../useAuth.hooks';
 import { Boulder } from '../types/boulder';
 
 export function Device() {
+
+  const { deviceId } = useParams();
+  const [ device, setDevice] = useState<DeviceCardProps>();
+
+  const navigate = useNavigate();
+
+  const fetchDevice = async () => {
+    if (!deviceId) return;
+
+    const { data: props } = await API.get<DeviceCardProps>(`/device/${deviceId}`);
+    setDevice(props);
+
+  };
+
+  useEffect(() => {
+    fetchDevice();
+  }, [deviceId]);
+
+  if (!device) return <></>;
+  const { id, name, ipAddress, image, deviceStatus } = device;
+
   return (
     <div className="flex justify-center">
       <div className="container">
@@ -13,6 +34,7 @@ export function Device() {
           <div className="divider" />
         </header>
         <main>
+          <DeviceCard id={id} name={name} ipAddress={ipAddress} deviceStatus={deviceStatus} image={image} />
         </main>
       </div>
     </div>
