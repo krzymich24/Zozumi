df45001bd7a79bf92ee4d128714f01bae5abfa26
krzymich24 <krzysztof.michalak1999@gmail.com>
2024-02-13 19:51:15 +0100

    testing api

--- api/src/database.provider.ts
+++ api/src/database.provider.ts
@@ -1,23 +1,14 @@
 import { join } from 'path';
 import { DataSource } from 'typeorm';
-import { networkInterfaces } from 'os';
 
 export const DATA_SOURCE = 'DATA_SOURCE';
 export const databaseProvider = [
   {
     provide: DATA_SOURCE,
     useFactory: async () => {
-      // Determine the IP address of the chosen network interface
-      const chosenInterface = 'enp0s3'; // Change this to your desired network interface name
-      const interfaces = networkInterfaces();
-      const chosenNetwork = interfaces[chosenInterface].find(
-        (iface) => !iface.internal && iface.family === 'IPv4'
-      );
-      const ipAddress = chosenNetwork ? chosenNetwork.address : 'localhost'; // Fallback to localhost if chosen interface not found
-
       const dataSource = new DataSource({
         type: 'postgres',
-        host: ipAddress,
+        host: 'localhost',
         port: 5432, // fixme: var
         username: 'root',
         password: 'root',
