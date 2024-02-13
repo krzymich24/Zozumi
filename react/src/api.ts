4238f0543a5240cea55519f4f162c1e6c2fd8daa
krzymich24 <krzysztof.michalak1999@gmail.com>
2024-02-13 20:46:20 +0100

    Update api.ts

--- react/src/api.ts
+++ react/src/api.ts
@@ -1,9 +1,21 @@
 import axios from 'axios';
+import os from 'os';
 
-const ipAddress = '192.168.1.13';
-console.log(ipAddress); // Log the IP address to console
+// Get the local IP address dynamically for the 'enp0s3' interface
+const networkInterfaces = os.networkInterfaces();
+const interfaceInfo = networkInterfaces['enp0s3'];
+let localIpAddress;
+
+if (interfaceInfo && interfaceInfo.length > 0) {
+  localIpAddress = interfaceInfo[0].address;
+} else {
+  console.error("Unable to find IP address for interface enp0s3");
+  process.exit(1); // Exit the process if IP address is not found
+}
+
+console.log(localIpAddress); // Log the IP address to console
 
 // Now you can use the IP address in your axios configuration
 export const API = axios.create({
-  baseURL: `http://${ipAddress}:3000`,
+  baseURL: `http://${localIpAddress}:3000`,
 });
