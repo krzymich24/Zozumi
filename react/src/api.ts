52ed578d06eb1c7d368b11475050d98f5c7a3b4a
krzymich24 <krzysztof.michalak1999@gmail.com>
2024-02-13 19:54:09 +0100

    Update api.ts

--- react/src/api.ts
+++ react/src/api.ts
@@ -1,22 +1,5 @@
 import axios from 'axios';
-import { networkInterfaces } from 'os';
 
-// Function to find the IP address associated with the specified network interface
-function getIPAddress(interfaceName) {
-  const interfaces = networkInterfaces();
-  if (interfaces && interfaces[interfaceName]) {
-    const iface = interfaces[interfaceName].find(iface => iface.family === 'IPv4');
-    if (iface) {
-      return iface.address;
-    }
-  }
-  return 'localhost'; // Fallback to localhost if interface or IP address not found
-}
-
-// Use the function to get the IP address of the specified network interface
-const ipAddress = getIPAddress('enp0s3');
-
-// Create an instance of Axios with the obtained IP address as baseURL
 export const API = axios.create({
-  baseURL: `http://${ipAddress}:3000`,
+  baseURL: 'http://192.168.1.13:3000',
 });
